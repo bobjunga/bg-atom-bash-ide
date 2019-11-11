8ceaef1e4f70a8698e3702d198a47b8ad683f568
Robert E. Griffith <bobg@avature.net>
2019-11-11 15:48:47 -0500

    initial commit

---
+++ spec/bg-atom-test-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BgAtomTestView from '../lib/bg-atom-test-view';
+
+describe('BgAtomTestView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
